d93a762a79496b565320c0a7a4bd123499ec7d2e
Robert Valta <robert.j.valta@gmail.com>
2018-09-14 14:04:30 +0300

    Simplify migrations

--- migrations/2_deploy_tokens.js
+++ migrations/2_deploy_tokens.js
@@ -1,7 +1,5 @@
 const EIP20 = artifacts.require('./EIP20.sol');
 
 module.exports = (deployer) => {
-  deployer.then(() => EIP20.new(10000, 'Simon Bucks', 1, 'SBX')).then((instance) => {
-    console.log(`|${instance.address}|`);
-  });
+  deployer.deploy(EIP20, 1000000000000, 'Human Token', 18, 'HMT')
 };
--- migrations/3_deploy_factory.js
+++ migrations/3_deploy_factory.js
@@ -1,18 +1,5 @@
-const fs = require('fs');
+const EIP20Factory = artifacts.require('./EIP20Factory.sol');
 
-const EIP20Factory =
-  artifacts.require('./EIP20Factory.sol');
 module.exports = (deployer) => {
-  deployer.then(() => EIP20Factory.new()).then((instance) => {
-    console.log(`|${instance.address}|`);
-    return EIP20Factory.at(instance.address).tokenAddress();
-  }).then((token) => {
-    console.log(`Deployed token to:${token}|`);
-    if (fs.existsSync('/account')) {
-      fs.writeFile('/account/account.txt', token, (err) => {
-        if (err) throw err;
-        console.log('Wrote! out account file');
-      });
-    }
-  });
+  deployer.deploy(EIP20Factory);
 };
