d9a888b4a5ad4538d3609fb2f1ccef3b9923e606
Robert Valta <robert.j.valta@gmail.com>
2018-09-25 18:00:43 +0300

    Adjust totalSupply decimals

--- contracts/HMToken.sol
+++ contracts/HMToken.sol
@@ -27,11 +27,11 @@ contract HMToken is HMTokenInterface {
     address public escrowFactory = 0;
 
     constructor(uint256 _totalSupply, string _name, uint8 _decimals, string _symbol) public {
-        totalSupply = _totalSupply;
+        totalSupply = _totalSupply * (10 ** uint256(_decimals));
         name = _name;
         decimals = _decimals;
         symbol = _symbol;
-        balances[msg.sender] = _totalSupply;
+        balances[msg.sender] = totalSupply;
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
