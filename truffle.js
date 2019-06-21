e445282e64294c891bce2dafba989dc63c1b08fb
Robert Valta <robert.j.valta@gmail.com>
2019-06-22 01:30:10 +0300

    Add explicit 0.5.9 compiler

--- truffle.js
+++ truffle.js
@@ -1,31 +1,60 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const { INFURA_TOKEN, MNEMONIC, ETH_HOST, ETH_PORT } = process.env;
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
   networks: {
     development: {
-      host: ETH_HOST || '127.0.0.1',
+      host: ETH_HOST || "127.0.0.1",
       port: ETH_PORT || 8545,
-      network_id: '*',
+      network_id: "*"
     },
     live: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://mainnet.infura.io/${INFURA_TOKEN}`),
-      network_id: '1',
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://mainnet.infura.io/${INFURA_TOKEN}`
+        ),
+      network_id: "1"
     },
     kovan: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://kovan.infura.io/${INFURA_TOKEN}`),
-      network_id: '2',
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://kovan.infura.io/${INFURA_TOKEN}`
+        ),
+      network_id: "2"
     },
     ropsten: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://ropsten.infura.io/${INFURA_TOKEN}`),
-      network_id: '3',
-      gas: 4700000,
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://ropsten.infura.io/${INFURA_TOKEN}`
+        ),
+      network_id: "3",
+      gas: 4700000
     },
     rinkeby: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://rinkeby.infura.io/${INFURA_TOKEN}`),
-      network_id: '4',
-    },
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://rinkeby.infura.io/${INFURA_TOKEN}`
+        ),
+      network_id: "4"
+    }
   },
+  compilers: {
+    solc: {
+      version: "0.5.9", // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+        evmVersion: "byzantium"
+      }
+    }
+  }
 };
